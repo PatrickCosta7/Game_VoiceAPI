368d0df1357dd80c0c0072ad7c4d66757800829c
Patrick Costa <pl.costa@hotmail.com>
2022-10-11 09:57:52 -0300

    Atualizando Projeto no GitHub

---
+++ js/validacao.js
@@ -0,0 +1,43 @@
+function verificaSeOChutePossuiUmValorValido(chute){
+    const numero = +chute;
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += '<div>Valor Inválido</div>'
+        return // Return é utilizado como uma espécie de Break para sair da função
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)){
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `<h2>Você acertou</h2>
+        <h3>O número secreto era: ${numeroSecreto}</h3> 
+        <button id="jogar-novamente" class="btn-jogar"></button>
+        `
+
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-angles-down></i> </div>`
+    } else if (numero < numeroSecreto) {
+        elementoChute.innerHTML += `<div>O número secreto é maior <i class="fa-solid fa-angles-up></i> </div>`
+    }
+
+}
+
+
+
+function chuteForInvalido(numero){
+    return Number.isNaN(numero); // NaN = NOTaNUMBER
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id = 'jogar-novamente'){
+        window.location.reload();
+    }
+    
+})
